bbcef4bb3a6309214fa79aa48bb8d3e2a37efa16
OMGye <1252078414@qq.com>
2018-08-02 16:51:31 +0800

    kuayu

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -73,7 +73,7 @@ public class UserController {
 
     @RequestMapping(value = "listunableuser.do",method = RequestMethod.GET)
     @ResponseBody
-    public ServerResponse<PageInfo> listUnAbleUser(HttpSession session,@RequestParam(value = "pageNum",defaultValue = "1") int pageNum, @RequestParam(value = "pageSize",defaultValue = "5") int pageSize){
+    public ServerResponse<PageInfo> listUnAbleUser(HttpSession session,@RequestParam(value = "pageNum",defaultValue = "1") int pageNum, @RequestParam(value = "pageSize",defaultValue = "5  ") int pageSize){
         User user = (User)session.getAttribute(Const.CURRENT_USER);
         if(user == null){
             return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
