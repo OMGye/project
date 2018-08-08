56e210ab7bf75ca52411806cfe24be0669e6d307
OMGye <1252078414@qq.com>
2018-08-08 15:13:41 +0800

    update

--- src/main/java/com/controller/MaterialController.java
+++ src/main/java/com/controller/MaterialController.java
@@ -112,7 +112,7 @@ public class MaterialController {
         if(user == null){
             return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
         }
-        return materialService.getUncheckList(itemId);
+        return materialService.getUncheckUseList(itemId);
     }
 
 
