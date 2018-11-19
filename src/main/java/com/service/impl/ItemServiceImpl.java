d315ae1c53e8451bbdddc4a912b543adc84b2216
OMGye <1252078414@qq.com>
2018-11-19 19:03:11 +0800

    update

--- src/main/java/com/service/impl/ItemServiceImpl.java
+++ src/main/java/com/service/impl/ItemServiceImpl.java
@@ -196,7 +196,7 @@ public class ItemServiceImpl implements ItemService{
                 logger.error("上传文件异常",e);
                 return null;
             }
-            if (item.getItemFile() != null && item.getItemFile() != "")
+            if (item.getItemFile() != null && item.getItemFile().equals(""))
             item.setItemFile(item.getItemFile()+","+PropertiesUtil.getProperty("ftp.server.http.prefix")+targetFile.getName());
             else
                 item.setItemFile(PropertiesUtil.getProperty("ftp.server.http.prefix")+targetFile.getName());
